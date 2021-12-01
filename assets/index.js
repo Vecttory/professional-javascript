9ac6233a78ba06e7e54a734ba26d40d4d1a2502c
Alberto Ola <alberto.ola.dev@gmail.com>
2021-12-01 12:47:14 -0600

    service workers

--- assets/index.js
+++ assets/index.js
@@ -9,3 +9,10 @@ const muteButton = document.getElementById('muteButton');
 const player = new MediaPlayer({ el: video, plugins: [new AutoPlay(), new AutoPause()] });
 button.onclick = () => player.togglePlay();
 muteButton.onclick = () => player.toggleMuted();
+
+if ('serviceWorker' in navigator) {
+    console.log('service worker found!!!!');
+    navigator.serviceWorker.register('/sw.js').catch(error => {
+        console.log(error.message);
+    })
+}
---
+++ sw.js
@@ -0,0 +1,41 @@
+self.addEventListener('install', event => {
+    event.waitUntil(precache());
+})
+
+self.addEventListener('fetch', event => {
+    const request = event.request;
+    if(request.method !== 'GET') {
+        return;
+    }
+
+    event.respondWith(cachedResponse(request));
+
+    event.waitUntil(updateCache(request));
+})
+
+const VERSION = 'v1';
+
+async function precache() {
+    const cache = await caches.open(VERSION);
+    return cache.addAll([
+        '/',
+        '/index.html',
+        '/assets/index.js',
+        '/assets/MediaPlayer.js',
+        '/assets/plugins/AutoPlay.js',
+        '/assets/plugins/AutoPause.js',
+        '/assets/waterfall.mp4',
+    ])
+}
+
+async function cachedResponse(request) {
+    const cache = await caches.open(VERSION);
+    const response = await cache.match(request);
+    return response || fetch(request);
+}
+
+async function updateCache(request) {
+    const cache = await caches.open(VERSION);
+    const response = await fetch(request);
+    return cache.put(request, response);
+}
