362b842cfd2e544bc90204556c116f3a2ccdb3ea
Alberto Ola <alberto.ola.dev@gmail.com>
2021-12-01 23:38:44 -0600

    Ads plugin added

--- assets/MediaPlayer.ts
+++ assets/MediaPlayer.ts
@@ -2,12 +2,21 @@ class MediaPlayer {
 
     media: HTMLVideoElement;
     plugins: Array<any>;
+    container: HTMLElement;
 
     constructor(config) {
         this.media = config.el;
         this.plugins = config.plugins || [];
+        this.initPlayer();
 
         this.initPlugins();
+    }
+
+    initPlayer() {
+        this.container = document.createElement('div');
+        this.container.style.position = 'relative';
+        this.media.parentNode.insertBefore(this.container, this.media);
+        this.container.appendChild(this.media);
     }
 
     togglePlay() {
@@ -37,7 +46,7 @@ class MediaPlayer {
             return this.media.muted = false;
         this.media.muted = true;
     }
-    
+
     private initPlugins() {
 
         const player = {
--- assets/index.ts
+++ assets/index.ts
@@ -1,12 +1,13 @@
 import MediaPlayer from "./MediaPlayer";
 import AutoPlay from "./plugins/AutoPlay";
 import AutoPause from "./plugins/AutoPause";
+import Ads from "./plugins/Ads";
 
 const video = document.querySelector('video');
 const button: HTMLElement = document.querySelector('button');
 const muteButton: HTMLElement = document.getElementById('muteButton');
 
-const player = new MediaPlayer({ el: video, plugins: [new AutoPlay(), new AutoPause()] });
+const player = new MediaPlayer({ el: video, plugins: [new AutoPlay(), new AutoPause(), new Ads()] });
 button.onclick = () => player.togglePlay();
 muteButton.onclick = () => player.toggleMuted();
 
